8c0a98da077411bdacc75fab628f35102bf17540
Matevž Vidovič <matevz.vidovic@gmail.si>
2025-10-14 17:33:10 +0200

    autosave

--- 000MoreSpecificSetups/WslSpecificSetup/aliasesAndSuch.sh
+++ 000MoreSpecificSetups/WslSpecificSetup/aliasesAndSuch.sh
@@ -8,8 +8,8 @@ alias wsl='cd /mnt/c/Users/'$windows_username'/Desktop/WSL'
 wsl
 
 # main dev aliases
-alias m="make"
-alias t="task"
+alias m='make'
+alias t='task'
 
 # git aliases
 alias g='git'
@@ -27,20 +27,32 @@ alias gf='git fetch --all --prune --progress'
 alias gri='git rebase -i'
 
 
+# git connect to origin remote repo
+alias gcon1='git init && git add . && git commit -m "Initial commit" && git remote add origin' # add your repo SSH URL after this command
+alias gcon2='git remote -v && git branch -M main && git push -u origin main && git push'
+
+
+# conda aliases
+alias cn='conda'
+alias ca='conda activate'
+alias ccr='conda create --name'
+alias cexp='conda env export > environment.yml'
+alias ccrf='conda env create -f environment.yml'
+
 # other dev aliases
-alias c='code .'
-alias ca="conda activate"
-alias pm="python3 -m"
+alias c='code'
+alias cx='code .'
+alias pm='python3 -m'
 alias d='docker'
 alias dc='docker-compose'
-alias pp="poetry run" # nice for short scripts you define in pyproject.toml
+alias pp='poetry run' # nice for short scripts you define in pyproject.toml
 
 
 
 # LLM CLI stuff
-alias cla="claude --dangerously-skip-permissions --allowedTools Bash,Read,Glob,Grep,WebFetch,WebSearch,TodoWrite,Task,BashOutput,KillShell"
-alias cox="codex --search -a on-failure --sandbox workspace-write"
-alias coxr="codex --search -a never --sandbox read-only"
+alias cla='claude --dangerously-skip-permissions --allowedTools Bash,Read,Glob,Grep,WebFetch,WebSearch,TodoWrite,Task,BashOutput,KillShell'
+alias cox='codex --search -a on-failure --sandbox workspace-write'
+alias coxr='codex --search -a never --sandbox read-only'
 
 
 # random aliases
